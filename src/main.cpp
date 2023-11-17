b57da39b5d3e0d6d0238b064a99896c7d18839f0
Daniil Shvalov <shvalovich@gmail.com>
2023-11-17 17:03:43 +0300

    refactor!: use cli11 instead of program options

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/program_options.hpp>
 #include <boost/url.hpp>
-#include <cstdlib>
+#include <boost/url/scheme.hpp>
+
+#include <CLI/CLI.hpp>
+
 #include <iostream>
 #include <string>
 
@@ -18,106 +20,141 @@ namespace net = boost::asio;
 namespace ssl = net::ssl;
 using tcp = net::ip::tcp;
 namespace urls = boost::urls;
-namespace po = boost::program_options;
+
+std::ostream& operator<<(std::ostream& out, urls::scheme scheme) {
+    return out << urls::to_string(scheme);
+}
+
+struct SchemeValidator : public CLI::Validator {
+   public:
+    SchemeValidator() {
+        name_ = "SCHEME";
+        func_ = [](const std::string& str) -> std::string {
+            if (urls::string_to_scheme(str) == urls::scheme::unknown) {
+                return "Scheme '" + str + "' is not one of the options: " +
+                       make_avaliable_options();
+            }
+            return "";
+        };
+        desc_function_ = []() -> std::string {
+            return make_avaliable_options();
+        };
+        non_modifying_ = true;
+    }
+
+   private:
+    static std::string make_avaliable_options() {
+        const int begin = static_cast<int>(urls::scheme::ftp);
+        const int end = static_cast<int>(urls::scheme::wss);
+
+        std::string result = "{";
+        for (int i = begin; i <= end; ++i) {
+            if (i != begin) {
+                result += ",";
+            }
+            result += urls::to_string(static_cast<urls::scheme>(i));
+        }
+        result += "}";
+
+        return result;
+    }
+};
+const static SchemeValidator Scheme;
 
 int main(int argc, char** argv) {
-    po::options_description desc("Allowed options");
+    CLI::App app{"clck — CLI for the link shortener clck.ru"};
+    app.get_formatter()->column_width(59);
 
-    // clang-format off
-    desc.add_options()
-        ("help,h", "print help info")
-        ("url,u", po::value<std::string>(), "url that needs to be shortened")
-        ("scheme,s", po::value<std::string>()->default_value("https"),
-         "which scheme set if it is missing in the url")
-        ("provider,p", po::value<std::string>()->default_value("clck.ru"),
-         "url of the clck link shortener")
-    ;
-    // clang-format on
+    std::vector<std::string> urls;
+    std::string scheme;
+    std::string provider;
 
-    po::positional_options_description p;
-    p.add("url", -1);
+    app.add_option("url", urls, "URL that needs to be shortened")->required();
+
+    app.add_option("-s,--scheme", scheme, "Which scheme set if it is missing")
+        ->type_name("SCHEME")
+        ->default_val("https")
+        ->check(Scheme);
+
+    app.add_option("-r,--remote", provider, "URL of the clck link shortener")
+        ->default_val("clck.ru");
 
     try {
-        po::variables_map vm;
-        po::store(po::command_line_parser(argc, argv)
-                      .options(desc)
-                      .positional(p)
-                      .run(),
-                  vm);
-        po::notify(vm);
-
-        if (vm.count("help") || argc <= 1) {
-            std::cout << desc << "\n";
-            return EXIT_SUCCESS;
-        }
-
-        urls::url long_url(vm["url"].as<std::string>());
-        if (!long_url.has_scheme()) {
-            long_url = urls::url(vm["scheme"].as<std::string>() + "://" +
-                                 std::string(long_url.buffer()));
-        }
-
-        urls::url url;
-        url.set_scheme("https");
-        url.set_host(vm["provider"].as<std::string>());
-        url.set_path("--");
-        url.set_query("url=" + urls::encode(long_url, urls::pchars));
-        const std::string host = url.host();
-        const auto scheme = url.scheme();
-        const char* port = scheme == "https" ? "443" : "80";
-        const auto target = url.path() + "?" + url.query();
-        const int version = 11;
-
-        net::io_context ioc;
-        ssl::context ctx(ssl::context::tlsv12_client);
-
-        tcp::resolver resolver(ioc);
-        beast::ssl_stream<beast::tcp_stream> stream(ioc, ctx);
-
-        if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
-            beast::error_code ec{static_cast<int>(::ERR_get_error()),
-                                 net::error::get_ssl_category()};
-            throw beast::system_error{ec};
-        }
-
-        auto const results = resolver.resolve(host, port);
-
-        beast::get_lowest_layer(stream).connect(results);
-
-        stream.handshake(ssl::stream_base::client);
-
-        http::request<http::string_body> req{http::verb::get, target, version};
-        req.set(http::field::host, host);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        http::write(stream, req);
-
-        beast::flat_buffer buffer;
-
-        http::response<http::string_body> res;
-
-        http::read(stream, buffer, res);
-
-        if (res.result() != http::status::ok) {
-            std::cerr << "API error:" << std::endl;
-            std::cerr << res.body() << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        std::cout << res.body() << std::endl;
-
-        beast::error_code ec;
-        stream.shutdown(ec);
-        if (ec == net::error::eof) {
-            ec = {};
-        }
-        if (ec) {
-            throw beast::system_error{ec};
-        }
-
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+        return app.exit(e);
     }
-    return EXIT_SUCCESS;
+
+    for (std::string long_url_raw : urls) {
+        try {
+            urls::url long_url(long_url_raw);
+            if (!long_url.has_scheme()) {
+                long_url =
+                    urls::url(scheme + "://" + std::string(long_url.buffer()));
+            }
+
+            urls::url url;
+            url.set_scheme("https");
+            url.set_host(provider);
+            url.set_path("--");
+            url.set_query("url=" + urls::encode(long_url, urls::pchars));
+            const std::string host = url.host();
+            const auto scheme = url.scheme();
+            const char* port = scheme == "https" ? "443" : "80";
+            const auto target = url.path() + "?" + url.query();
+            const int version = 11;
+
+            net::io_context ioc;
+            ssl::context ctx(ssl::context::tlsv12_client);
+
+            tcp::resolver resolver(ioc);
+            beast::ssl_stream<beast::tcp_stream> stream(ioc, ctx);
+
+            if (!SSL_set_tlsext_host_name(stream.native_handle(),
+                                          host.c_str())) {
+                beast::error_code ec{static_cast<int>(::ERR_get_error()),
+                                     net::error::get_ssl_category()};
+                throw beast::system_error{ec};
+            }
+
+            auto const results = resolver.resolve(host, port);
+
+            beast::get_lowest_layer(stream).connect(results);
+
+            stream.handshake(ssl::stream_base::client);
+
+            http::request<http::string_body> req{http::verb::get, target,
+                                                 version};
+            req.set(http::field::host, host);
+            req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+            http::write(stream, req);
+
+            beast::flat_buffer buffer;
+
+            http::response<http::string_body> res;
+
+            http::read(stream, buffer, res);
+
+            if (res.result() != http::status::ok) {
+                std::cerr << "API error:" << std::endl;
+                std::cerr << res.body() << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            std::cout << res.body() << std::endl;
+
+            beast::error_code ec;
+            stream.shutdown(ec);
+            if (ec == net::error::eof) {
+                ec = {};
+            }
+            if (ec) {
+                throw beast::system_error{ec};
+            }
+
+        } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
 }
